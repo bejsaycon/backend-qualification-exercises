10220f347164bc926c0499d13674b737df93ccfe
Bryan Jeff <bjsaycon@gmail.com>
2024-05-12 11:57:55 +0800

    fix: lvl1 problem2 lexi issues

--- src/level1/problem2/index.ts
+++ src/level1/problem2/index.ts
@@ -1,45 +1,41 @@
-import * as crypto from 'crypto';
+import * as crypto from "crypto";
 
-let randomByte: Buffer | null = null
-let count: number = 0;
+let randomByte: Buffer = crypto.randomBytes(4);
+let counterByte: Buffer = crypto.randomBytes(3);
+let count: number = parseInt(counterByte.toString("hex"), 16);
 
 export class ObjectId {
   private data: Buffer;
-  
 
   constructor(type: number, timestamp: number) {
-    /**
-     * insert your code here
-     */
-
-    const typeByte = Buffer.alloc(1)
-    typeByte.writeUInt8(type)
+    const typeByte = Buffer.alloc(1);
+    typeByte.writeUInt8(type, 0);
 
     const timestampByte = Buffer.alloc(6);
     for (let i = 5; i >= 0; i--) {
-        timestampByte.writeUInt8((timestamp >> (i * 8)) & 0xFF, i);
+      timestampByte.writeUInt8((timestamp >> (i * 8)) & 0xff, i);
     }
 
-    const counterByte = Buffer.alloc(3)
-    for (let i = 2; i >= 0; i--) {
-      counterByte.writeUInt8((count >> (i * 8)) & 0xFF, i);
-    }
-    count += 1
+    this.data = Buffer.concat([
+      typeByte,
+      timestampByte,
+      randomByte,
+      counterByte,
+    ]);
 
-    if (!randomByte) {
-      randomByte = crypto.randomBytes(4)
-    }
-
-    this.data = Buffer.concat([typeByte, timestampByte, randomByte, counterByte])
+    count += 1;
+    counterByte = Buffer.from([
+      (count >> 16) & 0xff,
+      (count >> 8) & 0xff,
+      count,
+    ]);
   }
-
-
 
   static generate(type?: number): ObjectId {
     return new ObjectId(type ?? 0, Date.now());
   }
-  
-  toString(encoding?: 'hex' | 'base64'): string {
-    return this.data.toString(encoding ?? 'hex');
+
+  toString(encoding?: "hex" | "base64"): string {
+    return this.data.toString(encoding ?? "hex");
   }
 }
